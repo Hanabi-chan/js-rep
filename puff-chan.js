ea5e05446499e219c41c6c99034157f71b1265a1
Hanabi-chan <80577155+Hanabi-chan@users.noreply.github.com>
2021-03-13 15:25:29 +0100

    Update puff-chan.js

--- puff-chan.js
+++ puff-chan.js
@@ -1,16 +1,12 @@
 //Puff-chan
-//skryti Puff-chan podle useragent
 jQuery(document).ready(function($){
     if (/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent)) {
       return
     }
-  
     var isShow = false, lock = false;
     var $btn = $('.back-to-top');
-  
     $(document).scroll(function () {
       if (lock) return
-  
       if ($(this).scrollTop() >= 700) {
         if (!isShow) $btn.addClass('load')
         isShow = true
@@ -21,25 +17,19 @@ jQuery(document).ready(function($){
         }
       }
     })
-  
     $btn.click(function () {
       lock = true
       $btn.addClass('ani-leave')
-  
       $("html, body").animate({ scrollTop: 0 }, 800);
-  
       setTimeout(function () {
         $btn.removeClass('ani-leave').addClass('leaved')
       }, 390)
-  //jak rychle se zobrazi puff
       setTimeout(function () {
         $btn.addClass('ending')
       }, 0)
-  
       setTimeout(function () {
         $btn.removeClass('load')
       }, 1000);
-  //jak dlouho bude videt puff
       setTimeout(function () {
         lock = false
         isShow = false
